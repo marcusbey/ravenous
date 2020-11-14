be87da671c7015d641e6c2324898dae913f8cd75
Romain BOBOE <rboboe@gmail.com>
2020-11-13 22:54:42 -0500

    Interacting with the Yelp API

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,26 +2,21 @@ import React from 'react';
 import './App.css';
 import BusinessList from '../BusinessList/BusinessList';
 import SearchBar from '../SearchBar/SearchBar';
-
-
-const business = {
-  imageSrc: 'https://content.codecademy.com/programs/react/ravenous/pizza.jpg',
-  name: 'MarginOtto Pizzeria',
-  address: '1010 Paddington Way',
-  city: 'Flavortown',
-  state: 'NY',
-  zipCode: '10101',
-  category: 'Italian',
-  rating: 4.5,
-  reviewCount: 90 
-  };
-
-const businesses = [business, business, business, business, business, business];
+import Yelp from '../../util/Yelp';
 
 export default class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      businesses: []
+    };
+    this.searchYelp = this.searchYelp.bind(this);
+  }
 
   searchYelp(term, location, sortBy) {
-    console.log(`Searching Yelp with ${term}, ${location}, ${sortBy}`);
+    Yelp.searchYelp(term, location, sortBy).then( (businesses) => {
+      return this.setState({ businesses: businesses });
+    });
   }
 
   render(){
@@ -29,7 +24,7 @@ export default class App extends React.Component {
       <div class="App">
         <h1>ravenous</h1>
         < SearchBar searchYelp = {this.searchYelp} />
-        < BusinessList businesses = {businesses }/>
+        < BusinessList businesses = {this.state.businesses }/>
     </div>
     );
   }
